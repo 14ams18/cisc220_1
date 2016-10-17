c38b190c73efa91aaaa0ad675075fd75023c31a4
14ams18 <abigael.schonewille@hotmail.com>
2016-10-17 06:30:19 -0400

    Working second script for Question4

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,51 +1,56 @@
 #!/bin/bash
 
-if [[ $1=="-s" ]] ; then
-	echo "Old \t Current \t File"
-	for line in $2 ; do
-		old=(awk '{print $1}' $line)
-		file=(awk '{print $2}' $line)
-		current=(ls -lh $file)
-		if [[ $old!=$current ]] ; then
-			echo "$old \t $current \t $file"
+if [[ "$1" == "-s" ]] ; then
+	echo $'Old \t\t Current \t File'
+	IFS=$'\n'
+	set -f
+	for line in $(cat "$2") ; do
+		old=${line:0:10}
+		file=${line:11}
+		current=$(ls -lh $file | awk '{print $1}')
+		if [[ "$old" != "$current" ]] ; then
+			echo -e "$old \t $current \t $file"
 		fi
 	done
-elif [[ $1=="-r" ]] ; then
-	for line in $2 ; do
-        	old=(awk '{print $1}' $line)
-                file=(awk '{print $2}' $line)
-                current=(ls -lh $file)
-                if [[ $old!=$current ]] ; then
+elif [[ "$1" == "-r" ]] ; then
+	IFS=$'\n'
+        set -f
+	for line in $(cat "$2") ; do
+        	old=${line:0:10}
+                file=${line:11}
+                current=$(ls -lh $file | awk '{print $1}')
+                if [[ "$old" != "$current" ]] ; then
 			permission=0
-			if [[ ${old:1:1}=='r' ]] ; then
-				(($permissions+=400))
+			if [[ "${old:1:1}" == "r" ]] ; then
+				permission=$(($permission+400))
 			fi
-			if [[ ${old:2:1}=='r' ]] ; then
-                                (($permissions+=200))
+			if [[ "${old:2:1}" == "w" ]] ; then
+                                permission=$(($permission+200))
 			fi
-			if [[ ${old:3:1}=='r' ]] ; then
-                                (($permissions+=100))
+			if [[ "${old:3:1}" == "x" ]] ; then
+                                permission=$(($permission+100))
 			fi
-			if [[ ${old:4:1}=='r' ]] ; then
-                                (($permissions+=40))
+			if [[ "${old:4:1}" == "r" ]] ; then
+                                permission=$(($permission+40))
 			fi
-			if [[ ${old:5:1}=='r' ]] ; then
-                                (($permissions+=20))
+			if [[ "${old:5:1}" == "w" ]] ; then
+                                permission=$(($permission+20))
 			fi
-			if [[ ${old:6:1}=='r' ]] ; then
-                                (($permissions+=10))
+			if [[ "${old:6:1}" == "x" ]] ; then
+                                permission=$(($permission+10))
 			fi
-			if [[ ${old:7:1}=='r' ]] ; then
-                                (($permissions+=4))
+			if [[ "${old:7:1}" == "r" ]] ; then
+                                permission=$(($permission+4))
 			fi
-			if [[ ${old:8:1}=='r' ]] ; then
-                                (($permissions+=2))
+			if [[ "${old:8:1}" == "w" ]] ; then
+                                permission=$(($permission+2))
 			fi
-			if [[ ${old:9:1}=='r' ]] ; then
-                                (($permissions+=1))
+			if [[ "${old:9:1}" == "x" ]] ; then
+                               permission=$(($permission+1))
 			fi
 			chmod $permission $file
                         echo "$file permissions reset from $current to $old"
                 fi
 	done
 fi
+
